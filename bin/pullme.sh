99a67593e29f57aecaf0c1058c854bde794dd4db
John Stein <john@johnandkerri.com>
2017-10-28 07:44:48 -0400

    Update pullme.sh

--- bin/pullme.sh
+++ bin/pullme.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
-# TODO
+#
+# Check and see if I have been updated.
+# If not, do nothing. If yes, run my deployment script.
+# 
+# We can run this every minute.
+# crontab -e
+# * * * * * ~/somewhere/bin/pullme.sh >/dev/null 2>&1
+
+MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+OUTPUT="$(git pull)"
+
+if [ "$OUTPUT" != "Already up-to-date." ]; then
+  $MY_DIR/deploy.sh
+fi
